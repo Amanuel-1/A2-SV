a532dbfe9ebf92f7912c113fb53231a413e443a5
Amanuel-1 <71079278+Amanuel-1@users.noreply.github.com>
2023-01-22 17:03:37 +0300

    Create Find _The_Kth_Largers_Integer.cpp

---
+++ Find _The_Kth_Largers_Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(), nums.end(), [](string& a , string& b)
+        {
+            if(a.length() == b.length())
+            {
+                return a < b;
+            }
+            else
+            {
+                return a.length() < b.length();
+            }
+        });
+        return nums[nums.size()-k];
+    }
+   
+};
