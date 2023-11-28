5b54b053f2cc8763854ef5909c22b7d80a6018b8
Amanuel-1 <71079278+Amanuel-1@users.noreply.github.com>
2023-11-28 19:23:33 +0300

    Added solution for Toeplitz Matrix

---
+++ onboarding/toeplitz-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        diag = matrix[0][0]
+        n = len(matrix)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i>0 and j>0 and matrix[i-1][j-1] != matrix[i][j]:
+                    print("this ",matrix[i][j], matrix[i-1][j-1])
+                    return False
+
+
+        return True
+
+
+[
+[11,74,0,93],
+[40,11,74,7]
+]
+
+        
