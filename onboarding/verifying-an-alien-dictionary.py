070ed4f5a4c6c4b2981a36f14ea1ccb9d4c651d4
Amanuel-1 <71079278+Amanuel-1@users.noreply.github.com>
2023-12-01 19:16:49 +0300

    Added solution for Verifying an Alien Dictionary

---
+++ onboarding/verifying-an-alien-dictionary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        orderInd = {c : i for i, c in enumerate(order)}
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return False
+                if w1[j] != w2[j]:
+                    if orderInd[w2[j]]<orderInd[w1[j]]:
+                        return False
+                    break
+
+        return True
