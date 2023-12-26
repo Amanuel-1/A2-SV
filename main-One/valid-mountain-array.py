5093989559643fbad57cdbcfeeaf2b87c6b0d31c
Amanuel-1 <71079278+Amanuel-1@users.noreply.github.com>
2023-12-26 15:23:30 +0300

    Added solution for Valid Mountain Array

---
+++ main-One/valid-mountain-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        i = 0
+
+        while i < n - 1 and arr[i] < arr[i + 1]:
+            i += 1
+
+        if i == 0 or i == n - 1:
+            return False
+
+        while i < n - 1 and arr[i] > arr[i + 1]:
+            i += 1
+
+        return i == n - 1
+            
