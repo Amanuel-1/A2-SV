ddcb186961698e6090f1eac5cd4c2bffdb268db8
Amanuel-1 <71079278+Amanuel-1@users.noreply.github.com>
2024-01-04 18:19:30 +0300

    Added solution for 3Sum

---
+++ phase2/3sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                sum_ = nums[i] + nums[j] + nums[k]
+                if sum_ == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif sum_ < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return result
